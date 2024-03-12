fe0b7e656c611a1da63752728986c1f8801a3a1d
Killian-Conyngham <killianconyngham@gmail.com>
2024-03-12 18:37:04 +0100

    Part b

--- circle.py
+++ circle.py
@@ -5,11 +5,14 @@ import numpy as np
 class Circle:
     def __init__(self, radius):
         self.radius = radius
-        self.area = np.pi * (radius ** 2)
-        self.perimeter = np.pi * 2 * radius
+        self.area_internal = np.pi * (radius ** 2)
+        self.perimeter_internal = np.pi * 2 * radius
+        #We already calculate the values for area and perimeter and area as attributes
     
     def area(self):
-        return(self.area)
+        return(self.area_internal)
+    #Returns the area
     
     def perimeter(self):
-        return(self.perimeter)
+        return(self.perimeter_internal)
+    #Returns the perimeter
--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 
 from helper import perform_calculation, convert_to_float
 
+from circle import Circle
+
 app = Flask(__name__)  # create the instance of the flask class
 
 
@@ -38,3 +40,24 @@ def calculate():
             return render_template('calculator.html', printed_result="You cannot divide by zero")
 
     return render_template('calculator.html')
+
+@app.route('/circle', methods=['GET', 'POST'])  # associating the GET and POST method with this route
+def circle():
+    if request.method == 'POST':
+        # using the request method from flask to request the values that were sent to the server through the POST method
+        radius1 = request.form['radius']
+
+        try:
+            radius1 = convert_to_float(value=radius1)
+        except ValueError:
+            return render_template('circle.html', printed_circle_result="Cannot perform operation with this input, perhaps it is not a number.")
+
+        if(radius1 >= 0):
+            Circle1 = Circle(radius=radius1)
+            Permimeter1 = Circle1.perimeter()
+            Area1 = Circle1.area()
+            return render_template('circle.html', printed_circle_result=f"The Perimiter of the circle is: {Permimeter1}. The Area is: {Area1}.")
+        else:
+            return render_template('circle.html', printed_circle_result="I am not allowed to calculate negative areas or perimeters.")
+
+    return render_template('circle.html')
