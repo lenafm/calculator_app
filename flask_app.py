1c5d5840325753f9ca8502491c4c23bbf4e321bb
Letícia Figueiredo Collado <70416391+lefcollado@users.noreply.github.com>
2024-03-13 16:12:18 +0100

    Leticia Homework 2 DSA

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-
 from helper import perform_calculation, convert_to_float
+from circle import Circle
 
 app = Flask(__name__)  # create the instance of the flask class
 
@@ -38,3 +38,44 @@ def calculate():
             return render_template('calculator.html', printed_result="You cannot divide by zero")
 
     return render_template('calculator.html')
+
+
+@app.route('/calculate_perimeter_area', methods=['GET', 'POST'])
+def calc_per_area():
+    perimeter_result = None
+    area_result = None
+
+    if request.method == 'POST':
+        radius = request.form['radius']
+
+        try:
+            radius = convert_to_float(radius)
+        except ValueError:
+            return render_template('circle.html', printed_result="Please enter a valid number for the radius.")
+
+        if radius <= 0:
+            return render_template('circle.html', printed_result="Radius must be a positive number.")
+
+        circle = Circle(radius)
+        perimeter_result = circle.calculate_perimeter()
+        area_result = circle.calculate_area()
+
+        print("Perimeter Result:", perimeter_result)
+        print("Area Result:", area_result)
+
+    return render_template('circle.html', perimeter_result=perimeter_result, area_result=area_result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#@app.route('/circle', methods=['GET', 'POST'])
+#def circle_perimeter():
+ #   if request.method == 'POST':
+  #      radius = float(request.form['radius'])
+   #     #circle = Circle(radius)
+        #perimeter = circle.calculate_perimeter()
+    #    perimeter = calculate_perimeter()  # Gayatri was here
+     #   return render_template('circle_perimeter.html', perimeter=perimeter)
+
+   # return render_template('circle_perimeter.html')
